8e494763882e823b4142e2f87bb8dbf7a9bfcb04
HowardRicardo <howard.medeiros@gmail.com>
2020-12-09 03:25:33 -0300

    fix: formatting bug fixed

--- src/store/reducers/thunk.js
+++ src/store/reducers/thunk.js
@@ -1,6 +1,7 @@
 import { getToken } from "./actions";
 
-export const getTokenThunk = (token) => {
+export const getTokenThunk = (tokenThunk) => {
+    const { token } = tokenThunk;
     return (dispatch, getState) => {
         if(token) {
             localStorage.setItem("token", token)
