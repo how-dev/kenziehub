81e92cc0f8ec2a1ce255a05af12bf32c5ce70077
Ivan Borba <mrborbaivan@gmail.com>
2020-12-10 09:45:56 -0300

    code otimization

--- src/pages/signUp/index.js
+++ src/pages/signUp/index.js
@@ -1,7 +1,18 @@
 import FormSignUp from "../../components/form/formSignUp/index";
+import { Grid } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  return <FormSignUp />;
+  return (
+    <>
+      <FormSignUp />
+      <Grid container justify="flex-end">
+        <Grid item xs={12}>
+          <Link to="/login">Already have an account? Sign in</Link>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default SignUp;
--- src/store/modules/token/reducer.js
+++ src/store/modules/token/reducer.js
@@ -1,15 +1,15 @@
 const theToken = {
-    key: localStorage.getItem("token") || "",
+  key: localStorage.getItem("token") || "",
 };
 
 const tokenReducer = (state = theToken, action) => {
-    switch(action.type) {
-        case "@TOKEN/newToken":
-            const { token } = action;
-            return { key: token };
-        default:
-            return state;
-    };
+  switch (action.type) {
+    case "@TOKEN/newToken":
+      const { token } = action;
+      return { key: token };
+    default:
+      return state;
+  }
 };
 
 export default tokenReducer;
