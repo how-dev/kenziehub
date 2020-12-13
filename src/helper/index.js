7f0a6a80ef4efcee0f0f256df726cb8e3376a1de
Jeferson <jefersonborba@protonmail.com>
2020-12-13 14:12:38 -0300

    added: settingsSchema added to the helper

--- src/helper/index.js
+++ src/helper/index.js
@@ -34,3 +34,37 @@ export const signUpSchema = yup.object().shape({
     .required("Por favor, informe um contato"),
   course_module: yup.string("formato inválido")
 });
+
+export const settingsSchema = yup.object().shape({
+  email: yup
+    .string("O formato do e-mail é inválido")
+    .email("O formato do e-mail é inválido")
+    .required("Por Favor, informe o e-mail"),
+  password: yup
+    .string("O formato da senha é inválido")
+    .min(6, "A Senha deve ter pelo menos 6 caracteres"),
+  name: yup
+    .string("O formato do nome é inválido")
+    .matches(
+      /\b[A-Za-zÀ-ú][A-Za-zÀ-ú]+,?\s[A-Za-zÀ-ú][A-Za-zÀ-ú]{2,19}\b/gi,
+      "Seu nome e sobrenome devem conter apenas letras"
+    )
+    .required("Por favor, informe seu nome"),
+  bio: yup
+    .string("O formato da bio é inválido")
+    .required("Por favor, fale um pouco sobre você"),
+  contact: yup
+    .string("O formato do contato é inválido")
+    .url("O formato do contato é inválido")
+    .required("Por favor, informe um contato"),
+  course_module: yup.string("formato inválido"),
+  title: yup
+    .string("O formato do título é inválido")
+    .required("Por favor, informe o título"),
+  description: yup
+    .string("O formato da descrição é inválido")
+    .required("Por favor, informe a descrição"),
+  deploy_url: yup
+    .url("O formato da url é inválido")
+});
+
