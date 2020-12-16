3a39e35aa6c57ffe3d953a2bca1558bac80ba8dc
Ivan Borba <mrborbaivan@gmail.com>
2020-12-16 14:04:19 -0300

    added the redux store for the users-list, also integrated it with the infinite scroll feature

--- src/requests/index.js
+++ src/requests/index.js
@@ -3,16 +3,7 @@ import axios from "axios";
 const baseUrl = "https://kenziehub.me/";
 
 export const SignUpRequest = (data, setResponse, setResponseError) => {
-    axios.post(`${baseUrl}users`, data).then((res) => {
-      res.id ? setResponse(res.id) : setResponseError(res.message);
-    });
-};
-
-export const usersRequest = (list, setList, page, haveNext, setHaveNext) => {
-  axios.get(`${baseUrl}users?perPage=20&page=${page}`).then((response) => {
-    setList([...list, ...response.data]);
-    if (response.data.length < 20) {
-      setHaveNext(!haveNext);
-    }
+  axios.post(`${baseUrl}users`, data).then((res) => {
+    res.id ? setResponse(res.id) : setResponseError(res.message);
   });
 };
